bca64f0f7fa88467c4f7bc5bb2901a465ab81782
dl2consulting <jian@dl2.se>
2014-02-27 07:37:05 -0800

    Type commit message here…

--- val2014.js
+++ val2014.js
@@ -11,7 +11,45 @@ $(document).ready(function(){
     slideMargin: 10
   });
 
-	
+$('.info-slider').bxSlider({
+    slideWidth: 228,
+    adaptiveHeight: false,
+    minSlides: 1,
+    maxSlides: 4,
+    slideMargin: 10
+  });	
+
+
+$(".fancybox")
+    .attr('rel', 'gallery')
+    .fancybox({
+        beforeShow: function () {
+            if (this.title) {
+                this.title += '<br />';
+                
+                // Add tweet button
+                this.title += '<a href="https://twitter.com/share" class="twitter-share-button" data-count="none" data-url="' + this.href + '">Tweet</a> ';
+                
+                // Add FaceBook like button
+                this.title += '<div class="fb-share-button right" data-href="'+this.href+'" data-type="button_count"></div>';
+	  }
+        },
+        afterShow: function() {
+
+        			FB.XFBML.parse();
+					twttr.widgets.load();
+          
+        },
+        helpers : {
+            title : {
+                type: 'inside'
+            }
+        }  
+    });
+
+
+
+
 
 
 });
